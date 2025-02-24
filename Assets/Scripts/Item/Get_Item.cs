d64d6b5cd74640d82efd9db06ca717898f391914
Suu <jsuu1997@gmail.com>
2025-02-24 18:02:51 +0900

    [Feat] 아이템 스크립트 추가 및 이펙트 추가 작업
    Get_Item 스크립트 생성
    아이템 오브젝트들에 Item 태그 설정
    플레이어가 해당 아이템과 충돌 시
    장착 아이템을 교체
    필드 아이템에 파티클 기능 추가

--- Assets/Scripts/Item/Get_Item.cs
+++ Assets/Scripts/Item/Get_Item.cs
@@ -1,10 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Get_Item : MonoBehaviour
 {
+    // 라이플, 사이드, 폭탄 오브젝트 생성
+    public GameObject itemGun;
     public GameObject itemRifle;
     public GameObject itemScythe;
     public GameObject itemBoom;
+
+    private GameObject currentItem; // 현재 장착한 아이템
+
+    public void Start()
+    {
+        // 시작할 때 기본 무기는 총
+        currentItem = itemGun;
+        EquipItem(currentItem);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        // 아이템 태그 오브젝트와 충돌한다면
+        if(other.CompareTag("Item"))
+        {
+            string itemName = other.gameObject.name;
+
+            if (itemName == "Rifle")
+            {
+                EquipItem(itemRifle);
+            }
+            else if (itemName == "Scythe")
+            {
+                EquipItem(itemScythe);
+            }
+            else if (itemName == "Boom")
+            {
+                EquipItem(itemBoom);
+            }
+            Destroy(other.gameObject);
+        }
+    }
+    public void EquipItem(GameObject newItem)
+    {
+        // 기존 아이템을 비활성화
+        if (currentItem != null)
+        {
+            currentItem.SetActive(false);
+        }
+        
+        // 새 아이템을 활성화
+        currentItem = newItem;
+        currentItem.SetActive(true);
+    }
+
 }
