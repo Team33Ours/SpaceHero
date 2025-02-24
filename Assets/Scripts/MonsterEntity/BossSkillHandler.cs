39e1310640a56017544b22e6cdcb65fc3939f288
Imperor <imperor0103@gmail.com>
2025-02-25 04:36:32 +0900

    [Feat] 기본적인 스킬의 구조

--- Assets/Scripts/MonsterEntity/BossSkillHandler.cs
+++ Assets/Scripts/MonsterEntity/BossSkillHandler.cs
@@ -8,15 +8,7 @@ using UnityEngine;
 /// </summary>
 public class BossSkillHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
