4156912262d95a8e5d622429b9c16a86dee4db3d
Imperor <imperor0103@gmail.com>
2025-02-25 03:17:04 +0900

    [Add] 보스몬스터의 스킬 Entity

---
+++ Assets/Scripts/Entity/BaseSkillHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 플레이어, 몬스터의 BaseSkill
+/// 2025.02.25.ImSeonggyun
+/// </summary>
+public class BaseSkillHandler : MonoBehaviour
+{
+
+
+}
