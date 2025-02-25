5ebb5a16b069dec6d3e99b2002da297223780327
ColdEgg98 <leytriss@gmail.com>
2025-02-25 19:21:23 +0900

    [ADD] 추가 코인 UI, 스테이지 UI, 일시정지 버튼 UI 추가
    내부 미구현

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,30 +2,29 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour
+public class UIManager : Singleton<UIManager>
+
+
 {
-    private static UIManager Instance;
-    private Slider hpBar;
-    private TextMeshProUGUI currentHpText;
+    public GameObject Coin;
+    private TextMeshProUGUI CoinText;
+    public GameObject Stage;
+    private TextMeshProUGUI StageText;
+    public GameObject Pause;
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-        else
-        {
-            Destroy(this.gameObject);
-        }
-        hpBar = GetComponentInChildren<Slider>();
-        currentHpText = GetComponentInChildren<TextMeshProUGUI>(true);
-    }
+    //코인
+    //    UIManager에서 인스턴스 만들어서 관리
+    //    코인 양 보여주는 TextMeshProUGUI 변수 생성
+    //    코인 늘어나는 메서드 AddCoin 작성
+    //    코인 감소하는 메서드 MinusCoin 작성
+    //스테이지
+    //    스테이지 보여주는 TextMeshProUGUI 변수 생성
+    //    레벨 생성자에서 스테이지 관리 쉽게끔 함수 작성
+    //퍼즈
+    //    버튼 달아서 게임 일시정지 함수(시간 스케일) 추가 후 인스펙터에서 할당
+    //    일시정지
+    //        게임 재개
+    //        메인메뉴
 
-    //public void UpdateHP()
-    //{
-    //    hpDifferenceCheck = TempPlayer.instance.Stat.currentHP;
-    //    HPSlider.value = TempPlayer.instance.Stat.currentHP;
-    //    HPText.text = $"{HPSlider.value:F0}";
-    //}
+
 }
