d25a3b21e607db99c3caf69c3d116dae33740726
ColdEgg98 <leytriss@gmail.com>
2025-02-25 19:32:27 +0900

    [Feat] 추가 층 수 올라갔을 때 호출 될 함수
    AcendStage()로 간단히 층 수 로그 찍음

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,29 +2,43 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+//코인
+//    UIManager에서 인스턴스 만들어서 관리
+//    코인 양 보여주는 TextMeshProUGUI 변수 생성
+//    코인 늘어나는 메서드 AddCoin() 작성
+//    코인 감소하는 메서드 MinusCoin() 작성
+//스테이지
+//    스테이지 보여주는 TextMeshProUGUI 변수 생성
+//    레벨 생성자에서 스테이지 관리 쉽게끔 함수 작성
+//    스테이지 늘어나는 메서드 ClearStage()
+//퍼즈
+//    버튼 달아서 게임 일시정지 함수(시간 스케일) 추가 후 인스펙터에서 할당
+//    일시정지
+//        게임 재개
+//        메인메뉴
+
 public class UIManager : Singleton<UIManager>
-
-
 {
-    public GameObject Coin;
+    public GameObject CoinUI;
     private TextMeshProUGUI CoinText;
-    public GameObject Stage;
+
+    public GameObject StageUI;
+    private int stageFloor; 
     private TextMeshProUGUI StageText;
-    public GameObject Pause;
+    
+    public GameObject PauseUI;
 
-    //코인
-    //    UIManager에서 인스턴스 만들어서 관리
-    //    코인 양 보여주는 TextMeshProUGUI 변수 생성
-    //    코인 늘어나는 메서드 AddCoin 작성
-    //    코인 감소하는 메서드 MinusCoin 작성
-    //스테이지
-    //    스테이지 보여주는 TextMeshProUGUI 변수 생성
-    //    레벨 생성자에서 스테이지 관리 쉽게끔 함수 작성
-    //퍼즈
-    //    버튼 달아서 게임 일시정지 함수(시간 스케일) 추가 후 인스펙터에서 할당
-    //    일시정지
-    //        게임 재개
-    //        메인메뉴
+    private void Awake()
+    {
+        //CoinText = CoinUI.GetComponent<TextMeshProUGUI>();
+        //StageText = StageUI.GetComponent<TextMeshProUGUI>();
+        stageFloor = 0;
+    }
 
-
+    public void AcendStage()
+    {
+        stageFloor++;
+        Debug.Log($"Stage: {stageFloor}");
+        //StageText.text = $"{stageFloor}";
+    }
 }
