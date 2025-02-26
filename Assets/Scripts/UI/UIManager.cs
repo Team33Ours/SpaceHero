1fc592959517fb1caa06f0666cc94f3287e68128
ColdEgg98 <leytriss@gmail.com>
2025-02-26 21:12:35 +0900

    [ADD] 룰렛 애니메이션 구현

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
 using DG.Tweening;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using System.Linq;
+using System;
+using System.Collections;
 
 public class UIManager : Singleton<UIManager>
 {
@@ -24,10 +27,14 @@ public class UIManager : Singleton<UIManager>
     public Button PauseUI;
 
     // Roulette
-    TempSkill[] allOfTempskills; // Resources의 모든 스킬을 읽는다.
+    TempSkill[] allOfTempSkills; // Resources의 모든 스킬을 읽는다.
+    TempSkill[] get3RandomSkills = new TempSkill[3];
+    public Image[] rouletteImages = new Image[3];
+    internal int moveCount;
+    internal System.Random setMoveTimes;
 
     // Test
-    float testTimer = 0;
+    public float duration = 0.8f;
 
     protected override void Awake()
     {
@@ -42,6 +49,11 @@ public class UIManager : Singleton<UIManager>
         StageText.text = $"{stageFloor}";
 
         PauseUI = GetComponent<Button>();
+
+        //
+        setMoveTimes = new System.Random();
+        moveCount = setMoveTimes.Next(5, 8);
+        Debug.Log($"반복 횟수: {moveCount}회");
     }
 
     private void Start()
@@ -68,17 +80,97 @@ public class UIManager : Singleton<UIManager>
          * 8. 선택한 스킬을 적용한다.
          * 9. Time.timeScale = 1로 맞춘다.
          */
-        // Resources/Skills 폴더에 있는 모든 TempSkill 타입 에셋을 읽음
-        allOfTempskills = Resources.LoadAll<TempSkill>("Skills");
+        Time.timeScale = 0;
 
-        if (allOfTempskills != null)
+        // Resources/Skills 폴더에 있는 모든 TempSkill 타입 에셋을 읽음
+        allOfTempSkills = Resources.LoadAll<TempSkill>("Skills");
+
+
+        StartCoroutine(RunRouletteSequence());
+
+        //for (int j = 0; j < 3; j++)
+        //{
+        //    rouletteImages[j].gameObject.transform.localPosition = new Vector3(rouletteImages[j].gameObject.transform.localPosition.x, 400, rouletteImages[j].gameObject.transform.localPosition.z);
+        //}
+    }
+
+    private IEnumerator RunRouletteSequence()
+    {
+        for (int i = 0; i < moveCount; i++)
         {
-            Debug.Log("tempSkills is not null");
-            foreach (var data in allOfTempskills)
-            {
-                Debug.Log($"Skill Name: {data.Name}");
-            }
+            // 1회 애니메이션 실행 및 완료 대기
+            yield return StartCoroutine(ImagesMoveRefeatCoroutine());
+
+            // 이미지 위치 초기화
+            ResetImageTransform();
+
+            Debug.Log($"OnComplete: {i + 1}회 완료");
         }
+        Debug.Log("룰렛 애니메이션 완료");
+
+        ResetImageTransform();
+        get3RandomSkills = Set3RandomSkills();
+        SetRouletteImages();
+
+        var tween1 = rouletteImages[0].transform.DOLocalMoveY(rouletteImages[0].transform.localPosition.y - 400f, duration).SetUpdate(true).SetEase(Ease.OutBounce);
+        var tween2 = rouletteImages[1].transform.DOLocalMoveY(rouletteImages[1].transform.localPosition.y - 400f, duration).SetUpdate(true).SetEase(Ease.OutBounce);
+        var tween3 = rouletteImages[2].transform.DOLocalMoveY(rouletteImages[2].transform.localPosition.y - 400f, duration).SetUpdate(true).SetEase(Ease.OutBounce);
+
+        Time.timeScale = 1; // 타임스케일 복원
+    }
+    private IEnumerator ImagesMoveRefeatCoroutine()
+    {
+        // 스킬 3개를 뽑음
+        get3RandomSkills = Set3RandomSkills();
+        // 뽑은 스킬을 이미지 UI에 적용
+        SetRouletteImages();
+
+        // 모든 애니메이션 동시 시작
+        var tween1 = rouletteImages[0].transform.DOLocalMoveY(rouletteImages[0].transform.localPosition.y - 800f, duration).SetUpdate(true).SetEase(Ease.OutCubic);
+        var tween2 = rouletteImages[1].transform.DOLocalMoveY(rouletteImages[1].transform.localPosition.y - 800f, duration).SetUpdate(true).SetEase(Ease.OutCubic);
+        var tween3 = rouletteImages[2].transform.DOLocalMoveY(rouletteImages[2].transform.localPosition.y - 800f, duration).SetUpdate(true).SetEase(Ease.OutCubic);
+
+        // 마지막 애니메이션 완료 대기
+        yield return tween3.WaitForCompletion();
+    }
+
+    private void ResetImageTransform()
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            rouletteImages[j].transform.localPosition = new Vector3(
+                rouletteImages[j].transform.localPosition.x,
+                400,
+                rouletteImages[j].transform.localPosition.z
+            );
+        }
+    }
+
+
+    private TempSkill[] Set3RandomSkills()
+    {
+        System.Random rng = new System.Random();
+        allOfTempSkills = allOfTempSkills.OrderBy(s => rng.Next()).ToArray(); // OrderBy와 Random을 사용하여 랜덤으로 섞은 배열을 만듦
+        
+        for (int i = 0; i < 3; i++)
+        {
+            get3RandomSkills[i] = allOfTempSkills[i];
+        }
+
+        return get3RandomSkills;
+    }
+
+    private void SetRouletteImages()
+    {
+        // 스프라이트 적용
+        rouletteImages[0].sprite = get3RandomSkills[0].Icon;
+        rouletteImages[1].sprite = get3RandomSkills[1].Icon;
+        rouletteImages[2].sprite = get3RandomSkills[2].Icon;
+
+        // 텍스트에 설명 적용
+        rouletteImages[0].GetComponentInChildren<TextMeshProUGUI>(true).text = get3RandomSkills[0].Name;
+        rouletteImages[1].GetComponentInChildren<TextMeshProUGUI>(true).text = get3RandomSkills[1].Name;
+        rouletteImages[2].GetComponentInChildren<TextMeshProUGUI>(true).text = get3RandomSkills[2].Name;
     }
 
     #region Coin
