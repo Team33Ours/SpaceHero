5b594ef4d66a50425a4cb2dac19a6549b4ba9761
Choi Jae Hyuck <cgear@naver.com>
2025-02-27 16:09:58 +0900

    ✨[Feat]: “사운드 효과 플레이어,몬스터”
    플레이어 무기발사 피격음
    몬스터 공격, 피격음

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    [SerializeField][Range(0f, 1f)] private float soundEffectVolume;
+    [SerializeField][Range(0f, 1f)] private float soundEffectPitchVariance;
+    [SerializeField][Range(0f, 1f)] private float musicVolume;
+
+    private AudioSource musicAudioSource;
+    public AudioClip musicClip;
+
+    public SoundSource soundSourcePrefab;
+    private void Awake()
+    {
+        base.Awake();
+        musicAudioSource = GetComponent<AudioSource>();
+        musicAudioSource.volume = musicVolume;
+        musicAudioSource.loop = true;
+    }
+
+    private void Start()
+    {
+        ChangeBackGroundMusic(musicClip);
+    }
+
+    public void ChangeBackGroundMusic(AudioClip clip)
+    {
+        musicAudioSource.Stop();
+        musicAudioSource.clip = clip;
+        musicAudioSource.Play();
+    }
+
+    public static void PlayClip(AudioClip clip)
+    {
+        SoundSource obj = Instantiate(Instance.soundSourcePrefab);
+        SoundSource soundSource = obj.GetComponent<SoundSource>();
+        soundSource.Play(clip, Instance.soundEffectVolume, Instance.soundEffectPitchVariance); 
+    }
+    
+}
